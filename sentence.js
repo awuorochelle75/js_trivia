6522c1dbbf6a694643e73fabf4943268322be667
Rochelle Awuor <awuorochelle75@gmail.com>
2026-06-30 12:47:48 +0300

    Adds secondstory

--- sentence.js
+++ sentence.js
@@ -1,9 +1,12 @@
-let adjective = "afraid";
-let noun= "cat";
-let verb = "running";
-let place = "house";
-let adjective2 = "wearily";
-let noun2 = "tuna";
+let adjective = "big";
+let noun= "toad";
+let verb = "swimming";
+let place = "river";
+let adjective2 = "thrilling ";
+let noun2 = " worms";
 
 let firstStory = "Once upon a time, there was a(n)"+ " "+  adjective + " " + noun+ "" + " who loved to eat" + " "+ noun2 + "." + " " +  "The" + " " +  noun + " " + "lived in a " + " " + place + " " + "and had " + "" + adjective2 + " " +  "nostrils that blew fire when it was" + " "+ verb + ".";
 console.log("First story:"  + firstStory);
+
+let secondStory =  "Once upon a time, there was a(n)"+ " "+  adjective + " " + noun+ "" + " who loved to eat" + " "+ noun2 + "." + " " +  "The" + " " +  noun + " " + "lived in a " + " " + place + " " + "and had " + "" + adjective2 + " " +  "nostrils that blew fire when it was" + " "+ verb + ".";
+console.log("Second story:" + secondStory);
