576adc2784b55350b719112d43eea14e73deaf49
Rochelle Awuor <awuorochelle75@gmail.com>
2026-06-30 12:19:42 +0300

    Creates the trivia questions

---
+++ trivia.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Candice";
+let botLocation = "Spain";
+let favoriteLanguage = "Javascript";
+
+console.log("My name is" + " " + botName + " " + "and I live on " + " " + botLocation + ".");
+
+console.log("My favorite programming language is" + " " + favoriteLanguage + ".");
+
+let codingFact = "Its a bit tricky learning it." + favoriteLanguage + " " + "is used to add functionality to a website";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " " + "requires one to learn it consistently";
+console.log(codingFact);
+codingFact = favoriteLanguage + " " + "is very user-friendly when you get to learn about it";
+console.log(codingFact);
+
+console.log("It was really fun sharing these facts with you. Goodbye! -" + ""+ botName + " " + "from" +  " " +  botLocation);
