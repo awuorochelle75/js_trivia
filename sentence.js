a16be29249d257b557d4cc864468eb98dabb4a3b
Rochelle Awuor <awuorochelle75@gmail.com>
2026-06-30 12:41:44 +0300

    Adds code to the file

---
+++ sentence.js
@@ -0,0 +1,9 @@
+let adjective = "afraid";
+let noun= "cat";
+let verb = "running";
+let place = "house";
+let adjective2 = "wearily";
+let noun2 = "tuna";
+
+let firstStory = "Once upon a time, there was a(n)"+ " "+  adjective + " " + noun+ "" + " who loved to eat" + " "+ noun2 + "." + " " +  "The" + " " +  noun + " " + "lived in a " + " " + place + " " + "and had " + "" + adjective2 + " " +  "nostrils that blew fire when it was" + " "+ verb + ".";
+console.log("First story:"  + firstStory);
